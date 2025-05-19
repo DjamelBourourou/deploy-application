217ecc055bb8f02cdfee900362baa032c162e72b
Djamel <djamelbourourou@gmail.com>
2025-05-19 18:06:59 +0200

    version2

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="app">
      <h1>Mon titre:Mes premiers pas en developpement React</h1> 
+     <h2>Le developpement:c'est serieux!</h2>
      <p>{"Soyons Zen!"}</p>
      <button onClick={()=>setMessage('Bouton cliqué!')}>Clic-me!</button>
       
